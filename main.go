19993fe937fdadcd3df223343a97841473a16e58
Takuya Ueda <uedatakuya@gmail.com>
2020-06-04 16:07:08 +0900

    Add version command

--- main.go
+++ main.go
@@ -3,8 +3,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"runtime"
+
 	"github.com/gcpug/zagane/passes/unstopiter"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
-func main() { singlechecker.Main(unstopiter.Analyzer) }
+// version of zagane
+const version = "v0.4.0"
+
+func main() {
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		fmt.Printf("zagane %s (%s)\n", version, runtime.Version())
+		return
+	}
+	singlechecker.Main(unstopiter.Analyzer)
+}
--- main_go112.go
+++ main_go112.go
@@ -3,10 +3,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"runtime"
+
 	"github.com/gcpug/zagane/zagane"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
+// version of zagane
+const version = "v0.4.0"
+
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		fmt.Printf("zagane %s (%s)\n", version, runtime.Version())
+		return
+	}
 	unitchecker.Main(zagane.Analyzers()...)
 }
